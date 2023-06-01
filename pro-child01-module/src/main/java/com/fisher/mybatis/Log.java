7af22054b2b3b835fa4adf4f0031f84affdf41e9
fisher <fisher@macarthy.com>
2023-06-01 19:46:41 +0800

    mybatis 导入日志依赖03 日志使用测试

---
+++ pro-child01-module/src/main/java/com/fisher/mybatis/Log.java
@@ -0,0 +1,15 @@
+package com.fisher.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+
+/**
+ * 使用日志测试
+ * @author fisher
+ * @version 1.0.1 2023/6/1 - 19:24
+ */
+public class Log {
+}
---
+++ pro-child01-module/src/test/java/com/fisher/mybatis/LogTest.java
@@ -0,0 +1,27 @@
+package com.fisher.mybatis;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 使用日志测试
+ * @author fisher
+ * @version 1.0.1 2023/6/1 - 19:24
+ */
+public class LogTest {
+    private Logger logger = LoggerFactory.getLogger(LogTest.class);
+
+    @Test
+    public void testLog(){
+        logger.info(""+LocalDate.now().toString());
+        logger.trace("what is trace ?");
+        logger.warn("WARNING...");
+        logger.debug("this is debugging...");
+        logger.error("there are some errors...");
+    }
+}

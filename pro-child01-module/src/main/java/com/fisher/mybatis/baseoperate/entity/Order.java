3c452c20b8b5434b38bd68ec80049b6fd1077f58
fisher <fisher@macarthy.com>
2023-06-02 16:46:50 +0800

    mybatis 映射关联关系 创建实体类和数据表

---
+++ pro-child01-module/src/main/java/com/fisher/mybatis/baseoperate/entity/Order.java
@@ -0,0 +1,22 @@
+package com.fisher.mybatis.baseoperate.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 订单实体类
+ * @author fisher
+ * @version 1.0.1 2023/6/2 - 16:38
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    //订单id
+    private Integer orderId;
+    //订单名
+    private String orderName;
+    //订单客户（一个订单对应一个客户）
+    private Customer customer;
+}

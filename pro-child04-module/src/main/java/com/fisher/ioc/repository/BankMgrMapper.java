187d4b822405c246d1e048233b7124d0facb75a2
fisher <fisher@macarthy.com>
2023-06-06 19:08:09 +0800

    some update

---
+++ pro-child04-module/src/main/java/com/fisher/ioc/repository/BankMgrMapper.java
@@ -0,0 +1,19 @@
+package com.fisher.ioc.repository;
+
+import com.fisher.bankmgr.pojo.BankMgrResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 持久化层
+ * @author fisher
+ * @version 1.0.1 2023/6/6 - 14:59
+ */
+@Repository
+public class BankMgrMapper {
+
+
+//    List<BankMgrResource> selectBankMgrResourceList();
+
+}

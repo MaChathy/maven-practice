9a94f815095794ae44d718a0ebf5973c8cb527dd
fisher <fisher@macarthy.com>
2023-06-10 14:09:08 +0800

    spring 整合junit测试

---
+++ pro-child04-module/src/test/java/com/fisher/ioc/JunitIntegrationSpring.java
@@ -0,0 +1,30 @@
+package com.fisher.ioc;
+
+import com.fisher.ioc.controller.BankMgrController;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 整合junit测试
+ *
+ * @author fisher
+ * @version 1.0.1 2023/6/10 - 13:59
+ */
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = "classpath:applicationContext.xml")
+public class JunitIntegrationSpring {
+
+    @Autowired()
+    private BankMgrController bankMgrController;
+
+    @Test
+    public void testIntegration(){
+        bankMgrController.getMessages();
+    }
+
+}

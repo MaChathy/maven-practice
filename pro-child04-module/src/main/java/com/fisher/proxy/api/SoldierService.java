8cda75147b33fc7daad82157998e84d7f9d47aa8
fisher <fisher@macarthy.com>
2023-06-11 13:38:12 +0800

    AOP_10 环绕通知

--- pro-child04-module/src/main/java/com/fisher/proxy/api/SoldierService.java
+++ pro-child04-module/src/main/java/com/fisher/proxy/api/SoldierService.java
@@ -9,12 +9,33 @@ import org.springframework.stereotype.Service;
  */
 public interface SoldierService {
 
+    /**
+     * 保存指定士兵名信息
+     * @param soldierName 士兵名
+     * @return 影响行数
+     */
     int saveSoldier(String soldierName);
 
+    /**
+     * 删除指定编号信息
+     * @param soldierId 指定编号
+     * @return 影响行数
+     */
     int removeSoldier(Integer soldierId);
 
+    /**
+     * 更新相关信息
+     * @param soldierId 指定编号
+     * @param soldierName 指定姓名
+     * @return 影响行数
+     */
     int updateSoldier(Integer soldierId, String soldierName);
 
+    /**
+     * 获取指定编号信息
+     * @param soldierId 指定编号
+     * @return 姓名
+     */
     String getSoldierNameById(Integer soldierId);
 
 }
---
+++ pro-child04-module/src/main/java/com/fisher/proxy/aspect/ServiceTransactionAspect.java
@@ -0,0 +1,62 @@
+package com.fisher.proxy.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * Service层 事务切入点
+ * @author fisher
+ * @version 1.0.1 2023/6/11 - 13:17
+ */
+@Slf4j
+@Aspect
+@Component
+public class ServiceTransactionAspect {
+
+    /**
+     * 使用@Around注解标明环绕通知方法
+     * @param joinPoint 通过在通知方法形参位置声明ProceedingJoinPoint类型的形参，Spring会将这个类型的对象传入
+     * @return blank
+     */
+    @Around(value = "com.fisher.proxy.pointcut.FisherPointCut.transactionPointCut()")
+    public Object manageTransaction(ProceedingJoinPoint joinPoint){
+
+        //获取外界调用目标方法时传入的形参数组
+        Object[] args = joinPoint.getArgs();
+
+        //获取目标方法的签名对象
+        Signature signature = joinPoint.getSignature();
+
+        //获取目标方法名
+        String methodName = signature.getName();
+
+        //声明目标方法的返回值
+        Object targetMethodReturnValue = null;
+
+        //开启事务（模拟）
+        try{
+            log.debug("[AOP环绕通知] 开启事务,方法名："+methodName+",参数列表："+ Arrays.asList(args));
+
+            // 通过ProceedingJoinPoint对象调用目标方法,并将目标方法的返回值传出
+            targetMethodReturnValue = joinPoint.proceed(args);
+
+            log.debug("[AOP环绕通知] 提交事务,方法名："+methodName+",方法返回值："+targetMethodReturnValue);
+
+        }catch (Throwable throwable){
+
+            log.debug("[AOP环绕通知] 回滚事务,方法名："+methodName+",异常信息："+throwable.getClass()+":"+throwable.getMessage());
+
+        }finally {
+
+            log.debug("[AOP环绕通知] 释放数据库连接,方法名："+methodName);
+        }
+
+        return targetMethodReturnValue;
+    }
+}

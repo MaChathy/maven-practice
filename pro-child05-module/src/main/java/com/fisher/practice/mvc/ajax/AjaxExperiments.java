6f1cd7f64d4a762c786542eba7dad2d74baefccc
fisher <fisher@macarthy.com>
2023-06-24 12:47:00 +0800

    Ajax to be continue...

--- pro-child05-module/src/main/java/com/fisher/practice/mvc/ajax/AjaxExperiments.java
+++ pro-child05-module/src/main/java/com/fisher/practice/mvc/ajax/AjaxExperiments.java
@@ -1,10 +1,9 @@
 package com.fisher.practice.mvc.ajax;
 
+import com.fisher.practice.mvc.entry.stu.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class AjaxExperiments {
 
     @ResponseBody
-    @RequestMapping("/ajax/experiment/One")
+    @RequestMapping(value = "/ajax/experiment/One")
     public String experiment01(
             @RequestParam(value = "userName") String userName,
             @RequestParam(value = "password") String password){
@@ -24,6 +23,16 @@ public class AjaxExperiments {
         log.debug("userName: " + userName);
         log.debug("password: " + password);
 
+        return "this message from handler as response[来自服务器]";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/ajax/experiment/Two")
+    public String experiment02(
+            @RequestBody Student student
+            ){
+
+        log.debug(student.toString());
         return "this message from handler as response[来自服务器]";
     }
 
